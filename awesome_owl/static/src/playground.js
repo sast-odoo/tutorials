831d1bb2f6942b0513e2a594d2f6143ff3224653
Samuel Struble <sast@odoo.com>
2024-02-09 13:41:00 -0500

    [ADD] New Card component that takes title and content properties

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,8 +1,10 @@
 /** @odoo-module **/
+
 import { useState, Component } from "@odoo/owl";
 import { Counter } from "./counter/counter";
+import { Card } from "./card/card";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
-    static components = { Counter }; // tell Playground what it will need for the awesome_owl.playground template
+    static components = { Counter, Card }; // tell Playground what it will need for the awesome_owl.playground template
 }
