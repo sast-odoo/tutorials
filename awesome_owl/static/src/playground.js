6fa6f5ce4ad459fce96593306468a8592b8347ac
Samuel Struble <sast@odoo.com>
2024-02-09 12:00:15 -0500

    [ADD] Counter button

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,7 +1,14 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { useState, Component } from "@odoo/owl";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
+    
+    state = useState({value: 0});
+
+    increment() {
+        this.state.value++;
+    }
+    
 }
